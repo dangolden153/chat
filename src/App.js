97489b64892cb4dc14c0bed1c0f58918c90b3d14
dangolden153 <dtosin153@gmail.com>
2020-11-19 02:41:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Chat from './chat.component'
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/chat.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Chat =()=>(
+    <div style ={{
+        height: '100vh',
+        background: 'pink',
+        textTransform: 'uppercase',
+
+
+    }}>
+        i'm a winner
+    </div>
+)
+export default Chat
